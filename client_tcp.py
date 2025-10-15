d59be4cd5d0977281d89e221dd98ee55ad4e6cb6
eduardo <eduardorc14@hotmail.com>
2025-10-15 17:57:31 -0300

    Add Organização dos Programs

---
+++ client_tcp.py
@@ -0,0 +1,8 @@
+/* Crie um programa que opere como cliente TCP/IP para ler e enviar dados remotamente ao
+processo (para fins pr´aticos, esse cliente pode rodar na mesmo m´aquina do servidor). Dados
+de controle e supervis˜ao devem ser exibidos ao operador via um sin´otipo gr´afico ou exibidas
+na pr´opria tela do terminal (a parte gr´afica ´e meramente opcional, mas vale pontos-extra).
+Todas essas informa¸c˜oes devem ser registradas em um arquivo denominado “historiador.txt”. O
+operador dever´a, via supervis´orio, escolher pontos de referˆencia para inspe¸c˜ao visual do drone,
+recebendo de volta os valores de posi¸c˜ao da robˆo e o timestamp associado. */
+
---
+++ clp.py
@@ -0,0 +1,6 @@
+/*Crie um programa chamado CLP que contenha um cliente OPC UA (para ler e atuar no drone
+via servidor) e um servidor TCP/IP (respons´avel por receber conex˜oes de clientes TCP/IP
+que desejem controlar o processo). Sugest˜ao: crie duas threads, uma para cada finalidade. O
+cliente OPC ir´a ler a posi¸c˜ao do drone (DroneX,Y,Z ) e enviar comandos de referˆencia de posi¸c˜ao
+(T argetX,Y,Z ), conforme ilustrado na figura 4*/
+
