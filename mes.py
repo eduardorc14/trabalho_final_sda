7f582c8aaa307324448ac4f7149ffa76f44cf234
Leonardo Fernandes <leonardodesarocha@gmail.com>
2025-10-20 19:03:03 -0300

    Uptade de configurações iniciais

--- mes.py
+++ mes.py
@@ -1,5 +1,5 @@
-/* Por fim, crie um segundo cliente OPCUA em outro programa, encapsulado dentro de outro
-servidor OPCUA, numa arquitetura que chamamos de chained server. O servidor fornecer´a as
-mesmas informa¸c˜oes do drone para outro cliente, chamado MES, que ir´a ler dados do processo
-para salvar em um arquivo chamado “mes.txt”. */
+# /* Por fim, crie um segundo cliente OPCUA em outro programa, encapsulado dentro de outro
+# servidor OPCUA, numa arquitetura que chamamos de chained server. O servidor fornecer´a as
+# mesmas informa¸c˜oes do drone para outro cliente, chamado MES, que ir´a ler dados do processo
+# para salvar em um arquivo chamado “mes.txt”. */
 
